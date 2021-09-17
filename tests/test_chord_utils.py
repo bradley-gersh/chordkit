b001abcaf0cfe4b0bff3dbaa42dbcd801ffd4340
Brad Gersh <bradley.gersh@gmail.com>
2021-09-17 13:21:35 +0200

    chord_utils tests corrected for OO usage

--- tests/test_chord_utils.py
+++ tests/test_chord_utils.py
@@ -1,7 +1,7 @@
 import unittest
-import chordkit.chord_utils as cu
 import pandas as pd
 from pandas.testing import assert_frame_equal
+import chordkit.chord_utils as cu
 
 class TestTimbre(unittest.TestCase):
     # test: make_timbre works with no amplitudes supplied
@@ -36,17 +36,19 @@ class TestMakeChord(unittest.TestCase):
         })
         assert_frame_equal(test_chord, cu.ChordSpectrum(chord_struct, 'ST_DIFF', timbre=timbre, fund_hz=fund).partials)
 
-#     # test: make_chord makes a chord using frequency ratios for intervals
-#     def test_make_chord_scale_factor(self):
-#         timbre = cu.make_timbre(range(1,3), [1/n for n in range(1,3)])
-#         chord_struct = [1, 2]
-#         test_chord = pd.DataFrame({
-#             'hz': [220, 440, 440, 880],
-#             'amps': [1.0, 0.5, 1.0, 0.5],
-#             'note_id': [0, 0, 1, 1],
-#             'fund_multiple': [1, 2, 1, 2],
-#         })
-#         assert_frame_equal(test_chord, cu.make_chord(chord_struct, 'SCALE_FACTOR', timbre))
+    # test: make_chord makes a chord using frequency ratios for intervals
+    def test_make_chord_scale_factor(self):
+        timbre = cu.Timbre(range(1,3), [1/n for n in range(1,3)])
+        chord_struct = [1, 2]
+        fund = 220
+        test_chord = pd.DataFrame({
+            'hz': [220., 440., 440., 880.],
+            'amp': [1.0, 0.5, 1.0, 0.5],
+            'note_id': [0, 0, 1, 1],
+            'fund_multiple': [1, 2, 1, 2],
+            'hz_orig': [220., 440., 440., 880.]
+        })
+        assert_frame_equal(test_chord, cu.ChordSpectrum(chord_struct, 'SCALE_FACTOR', timbre=timbre, fund_hz=fund).partials)
 
 if __name__ == '__main__':
     unittest.main()
